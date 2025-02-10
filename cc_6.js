dede12ca0aa12194de276ba77a76125d35fd06b3
Emma Adey <eadey@usf.edu>
2025-02-09 21:29:38 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -23,3 +23,23 @@ const calculateSalesTax = function(amount, taxRate) { // Calulating sales tax
 // Logging output
 console.log(calculateSalesTax(100, 0.07));
 console.log(calculateSalesTax(500, 0.1));
+
+// Task 3: Arrow Function
+console.log("\nTask 3- Arrow Functions");
+
+let calculateBonus = (salary, performanceRating) => {  // Arrow function calulating employee bonus 
+    let bonus; // declaring 'bonus' as variable 
+
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.20; // 20% bonus for "Excellent" performance rating
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.10; // 10% bonus for "Good" performance rating
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05; // 5% bonus for "Average" performance rating 
+    }
+    return bonus;
+};
+
+// Logging bonus calulations 
+console.log("Bonus: $", calculateBonus (5000, "Excellent")); // Bonus with "Excellent" rating and 5000 salary
+console.log("Bonus: $", calculateBonus (7000, "Good")); // Bonus with "Good" rating and 7000 salary 
